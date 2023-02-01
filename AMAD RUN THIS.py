65cc84c36feb8e01ebe5d1ba864e2bd58bd024eb
ziadev7 <80635373+ziadev7@users.noreply.github.com>
2023-02-01 21:22:00 +0000

    Create AMAD RUN THIS.py

---
+++ AMAD RUN THIS.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+ahbad = "aa0"
+why_im_better = [f"i am becoming better than {ahbad}",f"i am becoming stronger than {ahbad}",f"i am becoming more advanced than {ahbad}",f"i know you're scared {ahbad}",f"i know you're reading this {ahbad}",f"sleep with one eye open {ahbad}",f"i am scrooge. thats right i am on my redemption {ahbad}",f"who even is {ahbad}?"]
+while 2 > 1:
+  print(random.choice(why_im_better))
+  time.sleep(1)
+  
